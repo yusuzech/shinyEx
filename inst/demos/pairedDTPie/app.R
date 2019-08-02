e4e7e333afe003a6daa3ea0947b872424e5dffaf
Yifu Yan <yyan@ameritfleet.com>
2019-08-02 12:14:26 -0700

    pairedDTPie complete

--- inst/demos/pairedDTPie/app.R
+++ inst/demos/pairedDTPie/app.R
@@ -4,6 +4,7 @@ library(plotly)
 library(DT)
 library(reshape2)
 library(shiny)
+library(shinydashboard)
 source("../../../R/pairedDTPie.R")
 
 data <- reshape2::dcast(diamonds,clarity ~ color,value.var = "x",fun.aggregate = mean)
@@ -13,7 +14,10 @@ ui <- fluidPage(
 )
 
 server <- function(input, output, session) {
-  callModule(pairedDTPie,"test",11,1)
+  callModule(module = pairedDTPie,
+             id = "test",
+             df = data,
+             )
 }
 
 shinyApp(ui, server)
